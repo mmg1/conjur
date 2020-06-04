201e51cbb06672848f72fdcbfd47c67fbfa19659
Alon Carmel <alon.carmel@cyberark.com>
2020-06-04 18:31:35 +0300

    Update debify version
    The Debify Conjur image already contain ruby2.5 compiled with OpenSSL FIPS compliance

--- package.sh
+++ package.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -ex
-export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11'
+
+export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.4.0-04381b7'
 
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
+
+# Pulling private image explicitly and manually to local repository
+# We don't pass registry credentials to debify, remove this when registry.tld/ruby-fips-base-image-phusion:1.0.0 become public.
+docker pull registry.tld/ruby-fips-base-image-phusion:1.0.0
 
 ./docker-debify package \
   --dockerfile=Dockerfile.fpm \
